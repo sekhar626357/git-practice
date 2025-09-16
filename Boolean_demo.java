911cdafdf1ff139d6f5a86cc1fe2f69d84b2e539
Sekhar <annamgigourisekhar8054@gmail.com>
2025-09-16 11:01:54 +0530

    Create Boolean_demo.java
    Example of Boolean.

---
+++ Boolean_demo.java
@@ -0,0 +1,14 @@
+//Example of boolean
+public class Boolean_demo {
+ public static void main (String [] args){
+   boolean b;
+   b = false;
+  System.out.println("b is " + b);
+   b = true;
+  System.out.println("b is " + b);
+  if(b){
+        System.out.println("This is executed.");
+  }
+ }
+
+}
